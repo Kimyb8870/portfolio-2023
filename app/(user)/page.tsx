e9a188ee023a3922d149c4728e38e5158d7054af
yb kim <qhd0084@gmail.com>
2023-02-11 18:24:17 +0900

    Set: sanity preview mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "lib/sanity.client";
+import BlogList from "components/BlogList";
+import PreviewBlogList from "components/PreviewBlogList";
+import PreviewSuspense from "../../components/PreviewSuspense";
 
-function HomePage() {
-  return <div></div>;
+const query = groq`
+  *[_type=="post"]{
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+async function HomePage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense fallback={<div role="status">loading...</div>}>
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
+  }
+
+  const posts = await client.fetch(query);
+  return <BlogList posts={posts} />;
 }
 
 export default HomePage;
